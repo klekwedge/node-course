885df77b6abb5f4acc87061944a20c35a1f10e48
klekkwedge <antonov.andrey.10101@gmail.com>
2022-08-26 18:22:55 +0300

    feat: add 2 lesson

---
+++ Node JS/02 Global Objects/global.js
@@ -0,0 +1,19 @@
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log("Hello!");
+// }, 2000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(process);
+// console.log(process.env);
+// console.log(process.argv);
+// console.log(process.argv[2]);
+
+const url = new URL('https://google.com/path/name#example');
+console.log(url.hostname)
+console.log(url.href)
+console.log(url.pathname)
+console.log(url.hash)
