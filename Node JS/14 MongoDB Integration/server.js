a8ebbf2ac219f69584145ba38474fbe0525196d8
klekkwedge <antonov.andrey.10101@gmail.com>
2022-08-31 13:34:21 +0300

    feat: add 14 lesson

---
+++ Node JS/14 MongoDB Integration/server.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Post = require("./models/post");
+const app = express();
+
+app.set("view engine", "ejs");
+
+const PORT = 3000;
+const db =
+  "mongodb+srv://Klekwedge:hODVeVh6jbGMYT7H@cluster0.comel9l.mongodb.net/node-blog?retryWrites=true&w=majority";
+
+mongoose
+  // .connect(db)
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log("Connected to DB"))
+  .catch((error) => console.log(error));
+
+const createPath = (page) =>
+  path.resolve(__dirname, "ejs-views", `${page}.ejs`);
+
+app.listen(PORT, (error) => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("styles"));
+
+app.get("/", (req, res) => {
+  const title = "Home";
+  res.render(createPath("index"), { title });
+});
+
+app.get("/contacts", (req, res) => {
+  const title = "Contacts";
+  const contacts = [
+    { name: "YouTube", link: "#" },
+    { name: "Twitter", link: "#" },
+    { name: "GitHub", link: "#" },
+  ];
+  res.render(createPath("contacts"), { contacts, title });
+});
+
+app.get("/posts/:id", (req, res) => {
+  const title = "Post";
+  const post = {
+    id: "1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.",
+    title: "Post title",
+    date: "30.08.2022",
+    author: "Alex",
+  };
+  res.render(createPath("post"), { title, post });
+});
+
+app.get("/posts", (req, res) => {
+  const title = "Posts";
+  const posts = [
+    {
+      id: "1",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.",
+      title: "Post title",
+      date: "30.08.2022",
+      author: "Alex",
+    },
+  ];
+  res.render(createPath("posts"), { title, posts });
+});
+
+app.post("/add-post", (req, res) => {
+  const { title, author, text } = req.body;
+  const post = new Post({ title, author, text });
+
+  post
+    .save()
+    .then((result) => res.send(result))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath("error"), { title: "Error" });
+    });
+});
+
+app.get("/add-post", (req, res) => {
+  const title = "Add post";
+  res.render(createPath("add-post"), { title });
+});
+
+app.use((req, res) => {
+  const title = "Error Page";
+  res.status(404).render(createPath("error"), { title });
+});
