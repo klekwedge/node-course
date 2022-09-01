86437433a6e68f475a008816da48d6e347bf4bb7
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-01 21:45:05 +0300

    feat: add 18 lesson

--- Node JS/18 Create API/server.js
+++ Node JS/18 Create API/server.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const postRoutes = require("./routes/post-routes");
+const postApiRoutes = require("./routes/api-post-routes");
 const contactRoutes = require("./routes/contact-routes");
 const createPath = require("./helpers/create-path");
 
@@ -13,7 +14,6 @@ app.set("view engine", "ejs");
 const PORT = 3000;
 const db =
   "mongodb+srv://Klekwedge:hODVeVh6jbGMYT7H@cluster0.comel9l.mongodb.net/node-blog?retryWrites=true&w=majority";
-  
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((res) => console.log("Connected to DB"))
@@ -40,6 +40,7 @@ app.get("/", (req, res) => {
 
 app.use(postRoutes);
 app.use(contactRoutes);
+app.use(postApiRoutes);
 
 app.use((req, res) => {
   const title = "Error Page";
